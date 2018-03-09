cf70a2e7109f96a303d01f5eb683c34aa879d620
K. Thommy <kthommy@jehann.fr>
2018-03-09 15:36:25 +0100

    added login / logout links to navbar

--- src/Service/MenuManager.php
+++ src/Service/MenuManager.php
@@ -2,14 +2,40 @@
 
 namespace App\Service;
 
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
 class MenuManager
 {
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $checker;
+    
+    /**
+     * @param AuthorizationCheckerInterface $checker
+     */
+    public function __construct(AuthorizationCheckerInterface $checker)
+    {
+        $this->checker = $checker;
+    }
+    
+    /**
+     * @return array
+     */
     public function navbarLinks(): array
     {
-        return [
+        $links = [
             'app_main_index' => 'main.index.title',
             'app_worker_index' => 'worker.index.title',
             'app_job_index' => 'job.index.title',
         ];
+        
+        if ($this->checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $links['app_security_logout'] = 'security.action.logout';
+        } else {
+            $links['app_security_login'] = 'security.action.login';
+        }
+        
+        return $links;
     }
 }
